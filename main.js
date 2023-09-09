3bb75bd6396131fb50ec0cf8d15f6ea5b28cfa58
Alex Vranas <avranas42@gmail.com>
2023-09-09 09:25:26 -0700

    Complete project

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
-var contents = $("#contents");
-var contentsParent = contents.parent();
-contents.remove();
-contentsParent.append(
-  `<h2 class="beautText">Get back to building your dreams</h2>`
-);
-$.ajax({
-  method: "GET",
-  url: "https://picsum.photos/list",
-  success: function (result) {
-    randomNumber = Math.floor(Math.random() * 992);
-    const selection = result[randomNumber];
-    console.log(selection.id);
-    const postUrl = "https://unsplash.it/1200/800?image=" + selection.id;
-    console.log(postUrl);
-    contentsParent.append(`<img id="motivation" width=60% src=${postUrl}>`);
-  },
-  error: function (err) {
-    console.log("no success :(");
-    console.log(err);
-  },
-});
+document.querySelector("#sidebar").remove();
+document.querySelector("#left-sidebar").remove();
+document.querySelector("#mainbar").style.width = "100%";
